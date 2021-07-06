aa9bda473ba40b2d9fcbed8246985081c1992e69
christian <christianajulor@gmail.com>
2021-07-05 19:35:02 -0700

    new commits

--- ecomsite/ecomsite/settings.py
+++ ecomsite/ecomsite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l9(9gbl2!#mxxn02ky(!px#ou#==$m4-umdralp@+*$5h!=k6r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -127,4 +127,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
